b1f095addc3c09cc8ddd5c0c3821598dfc466f4c
Marissa Kalkar <mkalkar@wustl.edu>
2021-02-22 21:32:46 -0600

    added communication with backend

--- App.js
+++ App.js
@@ -2,14 +2,37 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
 export default function App() {
+  //fetch from backend
+  function Fetch(){
+  console.log("in fetch");
+  fetch('backend/AppBackend.js', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      firstParam: 'yourValue',
+      secondParam: 'yourOtherValue',
+    }),
+  });
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Hello Marissa wuz here</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <button onClick={() => Fetch()} type="button"> click here</button>
+
   );
 }
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
