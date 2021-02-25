c8455d99c8e89f29c63c6d4ed4d7392d38c1c710
mollyisaac <54511777+mollyisaac@users.noreply.github.com>
2021-02-25 12:52:13 -0600

    trying to get buttons

--- App.js
+++ App.js
@@ -1,25 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+//import { createBottomTabNavigator } from '@react-navigtion/bottom-tabs';
+import { Chat } from 'frontend/chat.js';
 
+/*function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
 
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}*/
+/*function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to chat"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   //fetch from backend
-  function Fetch(){
-  console.log("in fetch");
-  fetch('http://localhost:3000/', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json'
-      // 'Content-Type': 'application/json',
-      
-    },
-    body: JSON.stringify({
-      firstParam: 'yourValue',
-      secondParam: 'yourOtherValue',
-    }),
-  });
+  function Fetch() {
+    console.log("in fetch");
+    fetch('http://localhost:3000/', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json'
+        // 'Content-Type': 'application/json',
+
+      },
+      body: JSON.stringify({
+        firstParam: 'yourValue',
+        secondParam: 'yourOtherValue',
+      }),
+    });
   }
 
   function changePage() {
@@ -27,10 +80,13 @@ export default function App() {
   }
 
   return (
-    <div style={{backgroundColor: "blue"}}>
-      <div>MAIN PAGE</div>
-      <button onClick={() => changePage()} type="button"> click here</button>
-    </div>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
 
   );
 }
@@ -44,4 +100,43 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+});*/
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Chat" component={Chat} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
--- frontend/chat.js
+++ frontend/chat.js
@@ -1,18 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import App from '../App.js';
 
 
 
-export default function Chat() {
- 
-    function changePage() {
-        return res.redirect('/../App.js');
-      }
+export default function Chat({ navigation }) {
+
+  function changePage() {
+    return res.redirect('/../App.js');
+  }
 
   return (
-    <div style={{backgroundColor: "red"}}>
+    <div style={{ backgroundColor: "red" }}>
       <div>CHAT PAGE</div>
       <button onClick={() => changePage()} type="button"> click here to go back</button>
     </div>
