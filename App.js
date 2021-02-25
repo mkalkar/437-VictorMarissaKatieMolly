78eafee5cfa32c6f6cc9afb8801b50e36ede9d1b
mollyisaac <54511777+mollyisaac@users.noreply.github.com>
2021-02-25 15:40:07 -0600

    made lots of progress on bottom navigation bar -mi

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 import { Icon } from 'react-native-elements';
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import ChatScreen from './frontend/chat.js';
 import ProfilePage from './frontend/profile.js';
 import HomeScreen from './frontend/home.js';
@@ -18,7 +18,32 @@ const Tab = createBottomTabNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="Home">
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = 'home-outline';
+            } else if (route.name === 'Chat') {
+              iconName = 'chatbox-outline';
+            }
+            else {
+              iconName = 'person-circle-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+
+
+      >
         <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Home Page' }} />
         <Tab.Screen name="Chat" component={ChatScreen} />
         <Tab.Screen name="Profile" component={ProfilePage} />
